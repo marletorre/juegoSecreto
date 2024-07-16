b87a650c40770d75ced53932b08037b1a9952559
Marlene Torrealba <99843019+marletorre@users.noreply.github.com>
2024-07-16 17:08:22 -0300

    Version final del juego numero secreto

---
+++ app.js
@@ -0,0 +1,7 @@
+let diaSemana= prompt("Que dia de la semana es hoy?: ");
+
+if (diaSemana == "Sabado") {
+    alert("Buen fin de Semana!")
+} else if (diaSemana == "Domingo") {
+    alert("Buen fin de Semana!")
+}
